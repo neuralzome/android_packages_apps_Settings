7b19f4139622ba047d6cfc287b9149be860d00c4
Gagan Malvi <malvigagan@gmail.com>
2023-02-15 09:13:43 +0000

    [flo] Add device string
    Change-Id: I026cb0d1702508549f037511564aa0a2cf8ef4de
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/AboutDeviceNamePreferenceController.java
@@ -0,0 +1,34 @@
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class AboutDeviceNamePreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "AboutDeviceNameCtrl";
+
+    public AboutDeviceNamePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return Build.MODEL;
+    }
+}
